103ca0ae7f766d2aa1b390ba36ab3f5423e9db7c
Marcus <marcus.t.lindstedt@gmail.com>
2017-04-06 13:43:50 +0200

    made a change

--- src/index.php
+++ src/index.php
@@ -27,7 +27,7 @@ $app->get('/', function () use ($app) {
 $app->get('/insult', function () use ($app) {
 	$insult = get_insult(get_name(date('Y'), date('m'), date('d')));
 	$accept = $app->request->headers->get('ACCEPT');
-	
+
 	if ($accept == 'application/json') {
 		$app->response->headers->set('Content-Type', 'application/json');
 		$app->response->setBody(json_encode($insult));
@@ -47,7 +47,7 @@ $app->get('/insult/:year/:month/:day',
 	$title = $year . '-' . $month . '-' . $day . "'s insult";
 	$insult = get_insult(get_name($year, $month, $day));
 	$accept = $app->request->headers->get('ACCEPT');
-	
+
 	if ($accept == 'application/json') {
 		$app->response->headers->set('Content-Type', 'application/json');
 		$app->response->setBody(json_encode($insult));
@@ -63,7 +63,7 @@ $app->get('/insult/:year/:month/:day',
 $app->get('/insult/:name', function () use ($app) {
 	$insult = get_insult($name);
 	$accept = $app->request->headers->get('ACCEPT');
-	
+
 	if ($accept == 'application/json') {
 		$app->response->headers->set('Content-Type', 'application/json');
 		$app->response->setBody(json_encode($insult));
@@ -74,7 +74,7 @@ $app->get('/insult/:name', function () use ($app) {
 });
 
 /**
- * Produces an error message.
+ * Produces an error message.mwith ccame
  */
 $app->error(function(Exception $e) use ($app) {
 	$error = array(
@@ -82,7 +82,7 @@ $app->error(function(Exception $e) use ($app) {
 			'status' => 500,
 			'stack' => $e->getMessage()
 	);
-	
+
 	$app->render('error.tpl', $error, 500);
 });
 
@@ -91,7 +91,7 @@ $app->run();
 
 /**
  * Draws an insult at random from the pool of available FOAAS endpoints
- * 
+ *
  * @return A string containing an endpoint name.
  */
 function pick_insult() {
@@ -101,20 +101,20 @@ function pick_insult() {
 
 /**
  * Fetches an insult from the FOAAS web service.
- * 
+ *
  * @param $name A string containing a name.
  * @return A string with the fetched insult.
  */
 function get_insult($name) {
 	$client = new Guzzle\Http\Client();
-	
+
 	$url = "http://foaas.herokuapp.com/" . pick_insult() . "/" . $name;
 	$headers = array('Accept' => 'application/json');
 	$request = $client->get($url, $headers);
 	$response = $request->send();
 	$data = $response->json();
 	$insult = array('signed' => $name, 'message' => $data['message']);
-	
+
 	return $insult;
 }
 
@@ -122,7 +122,7 @@ function get_insult($name) {
  * Fetches today's name from the Svenska Dagar web service. The year variable
  * needs to be four digits long, while the month and day variables need to be
  * two digits long each. All three are expected to be strings.
- * 
+ *
  * @param $year The year as a four-digit string.
  * @param $month The month as a two-digit string.
  * @param $day The day as a two-digit string.
@@ -130,11 +130,11 @@ function get_insult($name) {
  */
 function get_name($year, $month, $day) {
 	$client = new Guzzle\Http\Client();
-	
+
 	$url = "http://api.dryg.net/dagar/v2/" . $year . "/" . $month . "/" . $day;
 	$request = $client->get($url);
 	$response = $request->send();
 	$data = $response->json();
-	
+
 	return array_pop($data['dagar'])['namnsdag'][0];
 }
